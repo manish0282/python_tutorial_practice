b2b90692166f29a0d47b0b667b9d99d4fd3f7173
mkumar01 <mkumar01@ivycomptech.com>
2018-08-12 13:28:54 +0530

    Added logic for repetition while entering data

--- firstSample/Lecture5.py
+++ firstSample/Lecture5.py
@@ -1,23 +1,26 @@
 class State():
 
     def __init__(self):
-        self.area = 0
-        self.name = "Telangana"
-        self.capital = "Hyderabad"
+        self.area = input("Enter Area of State ")
+        self.name = input("Enter State name ")
+        self.capital = input("Enter State Capital ")
 
 class Country():
 
     def __init__(self):
-        self.area = 0
-        self.name = "India"
-        self.capital = "New Delhi"
-        self.state_object= State()
+        self.area = input("Enter Area ")
+        self.name = input("Enter Country name ")
+        self.capital = input("Enter Capital name ")
+        self.number_of_states = input("Enter number of states ")
 
-    def president_name(self):
-        print("hello")
+        self.state_objects = []
+        for idx in range(0, int(self.number_of_states)):
+            print("Data being entered for state number" + str(idx))
+            state = State()
+            self.state_objects.append(state)
 
 person_object = Country()
-print(person_object.name + "\n" + person_object.state_object.name + "\n" + str(person_object.area) + "\n" + person_object.capital + "\n")
+print("\n" + person_object.name + "\n" + str(person_object.area) + "\n" + person_object.capital + "\n")
 
 # state_object = State()
 # print(str(state_object.area) + "\n" + state_object.name + "\n" + state_object.capital)
